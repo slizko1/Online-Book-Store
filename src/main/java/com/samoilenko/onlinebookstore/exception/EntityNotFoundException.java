dfa899912d33291f98f54ffd8f0caa788eecdfb0
Oleksandr Samoylenko <ntskazhu1985@gmail.com>
2024-04-23 23:01:46 +0300

    Fix

--- src/main/java/com/samoilenko/onlinebookstore/exception/EntityNotFoundException.java
+++ src/main/java/com/samoilenko/onlinebookstore/exception/EntityNotFoundException.java
@@ -4,6 +4,7 @@ public class EntityNotFoundException extends RuntimeException {
     public EntityNotFoundException(String message) {
         super(message);
     }
+
     public EntityNotFoundException(String message, Throwable cause) {
         super(message, cause);
     }
