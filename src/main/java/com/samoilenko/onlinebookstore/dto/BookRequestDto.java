c3b6766f5cdd5ed8efa1a073dfb9eae525ca6059
Oleksandr Samoylenko <ntskazhu1985@gmail.com>
2024-05-02 00:04:37 +0300

    Import's order was fixed

--- src/main/java/com/samoilenko/onlinebookstore/dto/BookRequestDto.java
+++ src/main/java/com/samoilenko/onlinebookstore/dto/BookRequestDto.java
@@ -1,11 +1,11 @@
 package com.samoilenko.onlinebookstore.dto;
 
-import org.hibernate.validator.constraints.ISBN;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import lombok.Data;
+import org.hibernate.validator.constraints.ISBN;
 
 @Data
 public class BookRequestDto {
