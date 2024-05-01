eaf189775a7e0c91157ccb7267f076a0cf64406a
Oleksandr Samoylenko <ntskazhu1985@gmail.com>
2024-05-02 00:00:13 +0300

    Some annotations for validating was added or changed

--- src/main/java/com/samoilenko/onlinebookstore/dto/BookRequestDto.java
+++ src/main/java/com/samoilenko/onlinebookstore/dto/BookRequestDto.java
@@ -1,19 +1,22 @@
 package com.samoilenko.onlinebookstore.dto;
 
+import org.hibernate.validator.constraints.ISBN;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
 public class BookRequestDto {
-    @NotNull
+    @NotBlank
     private String title;
     @NotNull
     @Min(0)
     private BigDecimal price;
-    @NotNull
+    @NotBlank
     private String author;
+    @ISBN
     private String isbn;
     private String description;
     private String coverImage;
