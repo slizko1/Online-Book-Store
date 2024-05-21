4a678e734105b151679fed09bb639c65b877fcbc
Oleksandr Samoilenko <81616606+slizko1@users.noreply.github.com>
2024-05-21 19:54:21 +0300

    Implement jwt support (#12)
    * The method login has been added to the AuthenticationController, which accepts a UserLoginRequestDto object as a parameter and returns a UserLoginResponseDto.
    The AuthenticationService, JwtAuthenticationFilter, and JwtUtil classes have been created.
    The JwtAuthenticationFilter has been integrated into the SecurityFilterChain bean.
    
    * Swagger annotation for login endpoint was added
    
    * Common fixes

---
+++ src/main/java/com/samoilenko/onlinebookstore/dto/UserLoginRequestDto.java
@@ -0,0 +1,16 @@
+package com.samoilenko.onlinebookstore.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
+@Data
+public class UserLoginRequestDto {
+    @NotBlank
+    @Email
+    private String email;
+    @NotBlank
+    @Length(min = 6, max = 35)
+    private String password;
+}
